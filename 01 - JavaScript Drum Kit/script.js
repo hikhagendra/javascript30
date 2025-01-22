fb44425644ad69b283df79b6b770260860dc5261
Khagendra Lama <khagendralama2@gmail.com>
2025-01-22 13:12:16 +0545

    Drum Kit task finished

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,38 @@
+let keys = document.querySelectorAll('.key');
+let audios = document.querySelectorAll('audio');
+
+window.addEventListener('keydown', function(event) {
+    let keyCode = event.keyCode;
+    
+    for(let key of keys) {
+        if(key.dataset.key == keyCode) {
+            highlighter(key);
+            playAudio(keyCode);
+        }
+    }
+});
+
+function playAudio(keyCode) {
+    for(let audio of audios) {
+        if(audio.dataset.key == keyCode) {
+            audio.load();
+            audio.play();
+        }
+    }
+}
+
+function highlighter(elem) {
+    for(let key of keys) {
+        if(key == elem) {
+            key.classList.add('playing');
+
+            windowdff.onkeyup = function() {
+                key.classList.remove('playing');
+            }
+        } else {
+            if(key.classList.contains('playing')) {
+                key.classList.remove('playing');
+            }
+        }
+    }
+}
